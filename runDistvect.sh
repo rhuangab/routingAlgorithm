fe320295c92612bb8dcc57ea8563eb6786b288f4
richeng <jerrcwong@gmail.com>
2014-11-23 01:58:55 -0600

    Update: runDistvect.sh to support stdio and file io option

--- runDistvect.sh
+++ runDistvect.sh
@@ -1,1 +1,7 @@
-./distvec mp3_topology.txt mp3_messages.txt mp3_changes.txt
+#!/bin/bash
+
+if [ $1 = '-stdio' ];then
+	./distvec stdio
+else
+	./distvec mp3_topology.txt mp3_messages.txt mp3_changes.txt
+fi
